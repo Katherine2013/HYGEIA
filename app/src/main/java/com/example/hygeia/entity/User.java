20b1fa2c20f5c38527f9b4e954f51740f68e8716
Katherine2013 <18604273487@qq.com>
2020-06-29 22:55:34 +0800

    Add: a 'User' model in ./entity folder

---
+++ app/src/main/java/com/example/hygeia/entity/User.java
@@ -0,0 +1,54 @@
+package com.example.hygeia.entity;
+
+import com.example.hygeia.entity.Task;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    // 用户的历史任务列表
+    protected List<Task> taskList;
+
+
+    // 构造类，只需要email和pwd
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
